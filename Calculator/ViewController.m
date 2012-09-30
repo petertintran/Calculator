0408b648d77ff883fd05df15b152441d25533647
petertintran <petertintran@gmail.com>
2012-09-29 20:07:49 -0700

    Incorp Buttons

--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -5,9 +5,7 @@
 //  Created by Lion User on 18/09/2012.
 //  Copyright (c) 2012 Tran.Peter. All rights reserved.
 //
-// Hello
-// Hello 2
-// Final Test
+
 
 
 #import "ViewController.h"
@@ -18,6 +16,69 @@
 
 @implementation ViewController
 
+@synthesize calcDisplay = _calcDisplay;
+
+- (IBAction)button0:(id)sender
+{}
+
+- (IBAction)button1:(id)sender
+{}
+
+- (IBAction)button2:(id)sender
+{}
+
+- (IBAction)button3:(id)sender
+{}
+
+- (IBAction)button4:(id)sender
+{}
+
+- (IBAction)button5:(id)sender
+{}
+
+- (IBAction)button6:(id)sender
+{}
+
+- (IBAction)button7:(id)sender
+{}
+
+- (IBAction)button8:(id)sender
+{}
+
+- (IBAction)button9:(id)sender
+{}
+
+- (IBAction)buttonPlus:(id)sender
+{}
+
+- (IBAction)buttonMinus:(id)sender
+{}
+
+- (IBAction)buttonMultiply:(id)sender
+{}
+
+- (IBAction)buttonDivide:(id)sender
+{}
+
+- (IBAction)buttonEqual:(id)sender
+{}
+
+
+- (IBAction)buttonRoot:(id)sender
+{}
+
+- (IBAction)buttonPosNeg:(id)sender
+{}
+
+- (IBAction)buttonDecimal:(id)sender
+{}
+
+- (IBAction)buttonClear:(id)sender
+{}
+
+
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -26,6 +87,7 @@
 
 - (void)viewDidUnload
 {
+    [self setCalcDisplay:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
